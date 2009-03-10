be281182f4df07decdd65d0cfadb2f52de186287
rjp <zimpenfish@gmail.com>
2009-03-10 21:34:45 +0000

    - work out positive and negative averages separately - plot min/max to compare with the original view of bjork - TODO plot min/max, avg on two canvases

--- cdcover.rb
+++ cdcover.rb
@@ -23,18 +23,25 @@ class Bucket
         @count = 0
         @min = 0
         @max = 0
+        @pos_total = 0
+        @neg_total = 0
     end
     def add(x)
         @total = @total + x
+        if x > 0 then
+            @pos_total = @pos_total + x
+        else
+            @neg_total = @neg_total + x
+        end
         @count = @count + 1
         if x > @max then @max = x; end
         if x < @min then @min = x; end
     end
     def avg
         if @count > 0 then
-           @total / @count
+            return @pos_total / @count, @neg_total / @count
         else
-            0
+            return 0, 0
         end
     end
 end
@@ -58,10 +65,15 @@ bytes.each_with_index { |i,j|
     buckets[bucket].add(left)
 }
 buckets.each_with_index { |b, i|
-    puts "#{i} #{b.min/scale} #{b.max/scale}"
+    # 261 -43.1272888183594 44.6958160400391 0.0112152099609375 / -26918 7 27897 70724 529807
     gc.stroke('#000000')
     if b.count > 0 then
-        gc.line(i, midpoint+b.min/scale, i, midpoint+b.max/scale)
+        avg_p, avg_n = b.avg
+        # 5 -6.80763244628906 8.06854248046875 -0.825119018554687 0.821914672851562 / -4249 -515 513 5036 70724 -88632
+        puts "#{i} #{b.min/scale} #{b.max/scale} #{avg_n/scale} #{avg_p/scale} / #{b.min} #{avg_n} #{avg_p} #{b.max} #{b.count} #{b.total}"
+        low = b.min # (avg_n + b.min) / 2
+        high = b.max # (avg_p + b.max) / 2
+        gc.line(i+offset, midpoint+low/scale, i+offset, midpoint+high/scale)
     end
 }
 gc.draw(canvas)
