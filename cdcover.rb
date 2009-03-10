027f4b51947a4d369f77ddc2cc22065e84fbfe00
rjp <zimpenfish@gmail.com>
2009-03-10 18:15:51 +0000

    - work out how many sample buckets we'll be plotting

--- cdcover.rb
+++ cdcover.rb
@@ -2,9 +2,11 @@ file = ARGV[0]
 x = IO.read(file)
 bytes = x.unpack("i*")
 test = bytes[-100..-1]
+p bytes.size
+bucket_size = bytes.size / width
+puts "#{bucket_size} samples in each bucket"
 test.each_with_index { |i,j|
     left = i >> 16
     right = i & 0xFFFF
     puts "#{j} #{left} #{right}"
 }
-p bytes.size
