802e72711214951e4ac28fba3ad10e29de4c0d2d
Rob Partington <zimpenfish@gmail.com>
2009-03-16 15:57:12 +0000

    - extract the ID3 info using id3info and some horribleness - TODO find something which does this nicely and doesn't make me cry

--- process.sh
+++ process.sh
@@ -1,12 +1,21 @@
 mkdir -p png
 x=1
 for i in "$@"; do
-    j=${i##*-}
-    k=${j%.*}
-# magically convert the input file to 16 bit linear PCM
-# also magically extract track information to set the title ($k)
-    ruby cdcover.rb "$i" "$x" "$k"
-    png=$(printf "%02d %s.png" $x "$k")
+    j=$(basename "$i")
+    k="${j%.*}"
+
+    # magically extract track information to set the title
+
+### THIS IS HORRIBLE
+    eval $(id3info "$i" | egrep 'TRCK|TIT2' | sed -e 's/^=== //' -e 's/"/\\"/g' -e 's/ ([^)]*): /="/' -e 's/$/"/')  
+    t=${TRCK:-$x}
+    trk=${t%/*}
+    ttl="${TIT2:-$k}"
+### THIS IS HORRIBLE
+
+    ruby cdcover.rb "$i" "$trk" "$ttl"
+
+    png=$(printf "%s.png" "$k")
     mv png/test.png png/"$png"
     x=$((x+1))
 done
